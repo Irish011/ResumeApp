13a0db138db1e641d218535f1bba7ec1272636e5
Irish <ajaybhartiirish@gmail.com>
2020-08-14 18:48:26 +0530

    Google

--- app/src/main/java/com/example/resumeapp/MainActivity.java
+++ app/src/main/java/com/example/resumeapp/MainActivity.java
@@ -5,13 +5,31 @@ package com.example.resumeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.submit);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://google.com/"));
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
